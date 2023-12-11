fb6398d2f4b43bcc9c1b05789ca39dde14629c0a
Javier <jmartin@grupoloyal.com>
2023-12-11 13:16:43 +0100

    calc

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        var calc = new Calculadora();
+        System.out.println(calc.add(1, 2));
+        System.out.println(calc.add(0.1, 0.2));
+        System.out.println(calc.add(1, -0.9));
     }
 }
