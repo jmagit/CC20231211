b2a74385ff9bb3f9eb0b55b5374797277e0b8f8e
Javier <jmartin@grupoloyal.com>
2023-12-12 09:06:42 +0100

    calculadora

--- demo/src/main/java/com/example/Calculadora.java
+++ demo/src/main/java/com/example/Calculadora.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Calculadora {
+	private double toDouble(double o) {
+		return (new BigDecimal(o))
+				.setScale(16, RoundingMode.HALF_UP)
+				.doubleValue();
+	}
+	
 	public double add(double a, double b) {
 		return a + b;
+	}
+	
+	public double divide(int a, int b) {
+		return a / b;
+	}
+	
+	public double divide(double a, double b) {
+		if(b == 0) 
+			throw new IllegalArgumentException("El divisor no puede ser 0");
+		return a / b;
 	}
 }
