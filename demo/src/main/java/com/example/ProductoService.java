f2fd1b59ff8a1688ae3583b1c9241e987b404ba9
Javier <jmartin@grupoloyal.com>
2023-12-12 13:16:17 +0100

    Producto

---
+++ demo/src/main/java/com/example/ProductoService.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public interface ProductoService extends DomainService<Producto, Integer> {
+
+}
---
+++ demo/src/test/java/com/example/AppicationServiceTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AppicationServiceTest {
+
+	@Test
+	void listaDeProducto() {
+		
+	}
+	
+	@Test
+	void unDeProducto() {
+		var srv = mock(ProductoService.class);
+		
+		
+	}
+}
